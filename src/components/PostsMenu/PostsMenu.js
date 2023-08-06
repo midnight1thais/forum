2550906c2fa29bd01124ce50c55e801d40c32caf
Thais <thaisplaneta2017@gmail.com>
2023-08-06 02:24:48 -0300

    criei menu

--- src/components/PostsMenu/PostsMenu.js
+++ src/components/PostsMenu/PostsMenu.js
@@ -1,7 +1,67 @@
-function PostsMenu() {
-    return(
-        <>
-            
-        </>
-    )
-}
+// import React from 'react';
+
+// import { MenuContainer, MenuItem, MenuText, MenuLine} from './style'
+
+// function PostsMenu() {
+//     return(
+//         <>
+//     <MenuContainer>
+//       <MenuItem href="#" isSelected>
+//         {/* Coloque aqui o ícone SVG se necessário */}
+//         <MenuText>Página Principal</MenuText>
+//       </MenuItem>
+//       <MenuLine />
+//       <MenuItem href="#">
+//         {/* Coloque aqui o ícone SVG se necessário */}
+//         <MenuText>Minhas postagens</MenuText>
+//       </MenuItem >
+//       <MenuLine />
+//       <MenuItem href="#">
+//         {/* Coloque aqui o ícone SVG se necessário */}
+//         <MenuText>Minha conta</MenuText>
+//       </MenuItem>
+      
+//     </MenuContainer>
+//         </>
+//     )
+// }
+
+// export default PostsMenu
+
+import home from '../../assets/home.png'
+import note from '../../assets/note.png'
+import user from '../../assets/user.png'
+
+
+import React, { useState } from 'react';
+import { Icon, MenuContainer, MenuItem, MenuLine, MenuText } from './style';
+
+function Menu(){
+  const [selectedItem, setSelectedItem] = useState(0);
+
+  const handleItemClick = index => {
+    setSelectedItem(index);
+  };
+
+  return (
+    <MenuContainer>
+      <MenuItem href="#" isSelected={selectedItem === 0} onClick={() => handleItemClick(0)}>
+        <Icon src={home} alt=''/>
+        <MenuText>Página Principal</MenuText>
+        {selectedItem === 0 && <MenuLine className="MenuLine" isSelected />}
+      </MenuItem>
+      <MenuItem href="#" isSelected={selectedItem === 1} onClick={() => handleItemClick(1)}>
+        <Icon src={note} alt=''/>
+        <MenuText>Minhas postagens</MenuText>
+        {selectedItem === 1 && <MenuLine className="MenuLine" isSelected />}
+      </MenuItem>
+      <MenuItem href="#" isSelected={selectedItem === 2} onClick={() => handleItemClick(2)}>
+        <Icon src={user} alt=''/>
+        <MenuText>Minha conta</MenuText>
+        {selectedItem === 2 && <MenuLine className="MenuLine" isSelected />}
+      </MenuItem>
+    </MenuContainer>
+  );
+};
+
+export default Menu;
---
+++ src/components/PostsMenu/style.jsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+
+export const MenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 2%;
+  top: 10%;
+`;
+
+export const MenuItem = styled.a`
+  display: flex;
+  padding: 10px;
+  border-radius: 10px 0 0 10px;
+  border-right: ${props => (props.isSelected ? '2px solid #00166A' : 'none')};
+  background: ${props => (props.isSelected ? '#C5D8FF' : 'transparent')};
+  transition: background 0.3s ease-in-out;
+  cursor: pointer;
+  text-decoration: none;
+  position: relative;
+
+  &:hover {
+    background: #C5D8FF;
+    border-right: 2px solid #00166A;
+  }
+
+  &:hover ~ .MenuLine {
+    background-color: #00166A;
+  }
+`;
+
+export const MenuLine = styled.div`
+  width: 2px;
+  height: 100%;
+  background-color: ${props => (props.isSelected ? '#00166A' : 'transparent')};
+  
+  top: 0;
+  visibility: ${props => (props.isSelected ? 'visible' : 'hidden')};
+`;
+
+export const Icon = styled.img`
+  width: 1rem; /* Use rem ou em para uma medida responsiva */
+  height: 1rem; /* Use rem ou em para uma medida responsiva */
+  
+`;
+
+export const MenuText = styled.span`
+  color: #000;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-left: 10px;
+`;
