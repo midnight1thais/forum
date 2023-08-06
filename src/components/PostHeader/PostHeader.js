86096d5569efc5222faffa8eeedbb48ff0399be3
Thais <thaisplaneta2017@gmail.com>
2023-08-05 23:19:06 -0300

    O header pronto

--- src/components/PostHeader/PostHeader.js
+++ src/components/PostHeader/PostHeader.js
@@ -1,30 +1,40 @@
+import React from 'react';
+
 import logo from '../../assets/Search.svg'
 import lupa from '../../assets/searchnormal.svg'
 import profile from '../../assets/profile.svg'
 
-import "./Estilo.css"
-import React from 'react';
+import { Header, ImgLupa, LinkA, Input, ImgLogo, ImgProfile, InputContainer} from './style';
+
 
 function PostHeader(){
-    return(
-        <>
-        <header>
-            <a>
-                <img src={logo} className='IMGlogo'/>
-            </a>
 
-            <div>
-                <a><img src={lupa} className='IMGlupa'/></a>
-                <input className='search' placeholder='Clique para pesquisar'></input>
-            </div>
-            
-            <a>
-                <img src={profile} className='IMGprofile'/>
-            </a>
-            
-        </header>
+    return(
+
+        <>
+        <Header>
+
+            <LinkA>
+                <ImgLogo src={logo} />
+            </LinkA>
+
+            <InputContainer>
+                <a><ImgLupa src={lupa}/></a>
+                <Input className='search' placeholder='Clique para pesquisar'/>
+            </InputContainer>
+
+           
+            <LinkA>
+                <ImgProfile src={profile}/>
+            </LinkA>
+
+        </Header>
         </>
+
     )
+
 }
+
+ 
 
 export default PostHeader
--- src/components/PostHeader/style.jsx
+++ src/components/PostHeader/style.jsx
@@ -1,6 +1,55 @@
 import styled from 'styled-components';
 
-export const Hearder = styled.header`
-    background-color = 0;
-    
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    width: 100vw;
+    height: 8vh;
+
 `
+
+export const LinkA = styled.a`
+
+`
+
+export const ImgLupa = styled.img`
+
+`
+
+export const ImgProfile = styled.img`
+    margin-top: 0.5vh;
+    width: 8vw;
+    height: 7vh;
+    margin-left: 5vw;
+
+`
+
+export const ImgLogo = styled.img`
+    margin-top: 0.5vh;
+    width: 8vw;
+    height: 7vh;
+    margin-right: 10vw;
+
+`
+
+export const Input = styled.input`
+    border: transparent;
+    border-style:none;
+    outline:none;
+    background-color: transparent;
+    ::placeholder{
+        color: #BBB;
+        font-family:sans-serif;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+`
+
+export const InputContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 10px; /* Espaçamento entre a imagem e o input */
+`;
