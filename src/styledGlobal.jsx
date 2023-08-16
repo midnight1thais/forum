3c5ed8be5ea5b310f717685ed5b33af45e3b6fbf
Thais <83969736+midnight1thais@users.noreply.github.com>
2023-08-15 21:03:51 -0300

    Exclusão do styledGlobal.jsx
    Foi preciso excluir esse styled, pois ele está incorreto (por estar com a letra minuscula)

--- src/styledGlobal.jsx
+++ src/styledGlobal.jsx
@@ -1,9 +1,1 @@
-import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle`
-    *{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
-`
