90322e256dd8d28df2812b7dc718425a26cb23ee
Thais <thaisplaneta2017@gmail.com>
2023-08-17 12:24:29 -0300

    adicionei a seção de botão

---
+++ src/components/AccountButtons/style.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ContainerButtons = styled.section`
+    display: flex;
+    flex-direction: row;
+    height: 20vh;
+    gap: 2vw;
+    background-color: green;
+`
+
+export const ButtonEdit = styled.button`
+    width: 5vw;
+    height: 5vh;
+    background-color: red;
+`
+
+export const ButtonPassword = styled.button`
+    width: 5vw;
+    height: 5vh;
+    background-color: pink;
+`
