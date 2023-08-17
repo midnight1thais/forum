b4189d622ca4966ac6810470727a6f14f388e59b
Thais <thaisplaneta2017@gmail.com>
2023-08-16 23:53:25 -0300

    Header do profile criado

---
+++ src/components/AccountProfileHeader/style.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    display: flex;
+    height: 100%;
+    width: 60vw;
+    background-color: red;
+`
+
+export const ImgProfile = styled.img`
+    margin-top: 0.5vh;
+    width: 16vw;
+    height: 16vh;
+    margin-right: 0.5vw;
+
+`
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 60%;
+    height: 30%;
+    gap: 1.5vw;
+    
+
+`
+
+export const Title = styled.h1`
+
+`
