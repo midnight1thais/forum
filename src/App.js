369a1a08013d9743dab62b749b50edd77f4f43cf
Thais <thaisplaneta2017@gmail.com>
2023-08-17 12:19:22 -0300

    comentei o que precisava

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import StyledGlobal from "./StyledGlobal";
 
-import LoginScreen from "./page/LoginScreen/LoginScreen";
-import RegisterScreen from "./page/RegisterScreen/RegisterScreen";
-import PostScreen from "./page/PostScreen/PostScreen";
+// import LoginScreen from "./page/LoginScreen/LoginScreen";
+// import RegisterScreen from "./page/RegisterScreen/RegisterScreen";
+// import PostScreen from "./page/PostScreen/PostScreen";
 import AccountScreen from "./page/AccountScreen/AccountScreen";
 
 
