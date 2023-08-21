08388a95759bc26c626b6fbc7068a504f291cfef
Thais <thaisplaneta2017@gmail.com>
2023-08-21 12:10:28 -0300

    criei a tela de edição

---
+++ src/page/AccountEditScreen/style.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.body`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 14vw;
+  margin-bottom: 5vh;
+`;
