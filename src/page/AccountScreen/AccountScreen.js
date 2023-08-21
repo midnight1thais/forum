d6c13d9778d3f166e2b716bfc33ee2f5c32b65d9
Thais <thaisplaneta2017@gmail.com>
2023-08-21 09:26:03 -0300

    mudei o caminho do header do profile

--- src/page/AccountScreen/AccountScreen.js
+++ src/page/AccountScreen/AccountScreen.js
@@ -2,7 +2,7 @@ import { Container, ContentWrapper,} from './style'
 
 import AccountHeader from "../../components/AccountHeader/AccountHeader"
 import PostsMenu from "../../components/PostsMenu/PostsMenu"
-import AccountProfileHeader from "../../components/AccountProfileHeader/AccountProfile"
+import AccountProfileHeader from "../../components/AccountProfileHeader/AccountProfileHeader"
 import AccountMain from "../../components/AccountMain/AccountMain"
 import AccountButtons from '../../components/AccountButtons/AccountButtons'
 
@@ -11,8 +11,8 @@ function AccountScreen(){
         <>
         <AccountHeader/>
         <Container>
+            <PostsMenu/>
             <ContentWrapper>
-                    <PostsMenu/>
                     <AccountProfileHeader/>
                     <AccountMain/>
                     <AccountButtons/>
