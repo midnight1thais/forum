f539590c60a109e8c3b686a00fa35cc980814eed
amanda-benites <amandabenites2005@gmail.com>
2023-11-02 20:02:31 -0300

    Adicionando os outros controllers na api relacionados a post

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,17 @@ const app             = express();
 require('dotenv').config();
 const usersRouter = require('./routes/usersRouter');
 const loginRouter = require('./routes/loginRouter');
+const postRouter = require('./routes/postsRouter');
+const commentRouter   = require('./routes/commentsRouter');
+const reactionsRouter = require('./routes/reactionsRouter');
 
 app.use(cors());
 app.use(express.json());
 app.set('port', process.env.PORT);
 app.use('/api', usersRouter);
 app.use('/api', loginRouter);
+app.use('/api', postRouter);
+app.use('/api', commentRouter);
+app.use('/api', reactionsRouter);
 
 module.exports = app;
