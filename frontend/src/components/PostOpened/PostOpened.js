e582b8874f8b85659ae7ed5bc975f5a8fae331d1
Thais <thaisplaneta2017@gmail.com>
2023-11-09 07:57:12 -0300

    coisas do comentário

--- frontend/src/components/PostOpened/PostOpened.js
+++ frontend/src/components/PostOpened/PostOpened.js
@@ -5,46 +5,44 @@ import addCircle from "../../assets/add-circle.svg"
 import axios from "axios";
 import { url } from "../../constants/url";
 import Comments from "../Comments/Comments";
+import { useEffect, useState } from "react";
 
 
 function PostOpened() {
     const [comments, setComments] = useState([]);
 
     useEffect(() => {
-        // Buscar informações de todos os usuários e armazenar em um objeto
-        axios.get(`${url.defaults.baseURL}/users`)
-            .then(function (response) {
-                const usersData = response.data.data;
-                const usersObject = {};
-                usersData.forEach(user => {
-                    usersObject[user.id] = user;
-                });
-                setUsers(usersObject);
-            })
-            .catch(function (error) {
-                console.log(error);
-                alert('Erro ao carregar informações dos usuários.');
-            });
-    }, []);
+      axios.get(`${url.defaults.baseURL}/comments/comments`)
+          .then(function (response) {
+            alert('Comentário cadastrado')
+          })
+          .catch(function (error) {
+              console.log(error);
+              alert("erro");
+          });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`${url.defaults.baseURL}/comments/:id`)
-            .then(function (response) {
-                const sortedPosts = response.data.data.sort((a, b) => {
-                    const dateA = new Date(a.created_at);
-                    const dateB = new Date(b.created_at);
-                    return dateB - dateA;
-                });
+  const [users, setUsers] = useState({});
 
-                setPosts(sortedPosts);
-            })
-            .catch(function (error) {
-                console.log(error);
-                alert("erro");
-            });
-    }, []);
 
-    const [users, setUsers] = useState({});
+  useEffect(() => {
+      // Buscar informações de todos os usuários e armazenar em um objeto
+      axios.get(`${url.defaults.baseURL}/users`)
+          .then(function (response) {
+              const usersData = response.data.data;
+              const usersObject = {};
+              usersData.forEach(user => {
+                  usersObject[user.id] = user;
+              });
+              setUsers(usersObject);
+          })
+          .catch(function (error) {
+              console.log(error);
+              alert('Erro ao carregar informações dos usuários.');
+          });
+  }, []);
+
+  
 
     return(
         <OpenedPostContainer>
@@ -68,6 +66,7 @@ function PostOpened() {
                         <h3>Comentários</h3>
                     </div>
                     <div>
+                        <input placeholder="Coloque seu comentário"/>
                         <ButtonAddComment><img src={addCircle} alt="Botão de adicionar comentário"/></ButtonAddComment>
                     </div>
                 </HeaderComments>
@@ -78,7 +77,6 @@ function PostOpened() {
                         titulo={comment.comment_name}
                         userIdValue={comment.userComment_id}
                         user={users[comment.userComment_id]}
-                        date={calculateTime(comment.created_at)}
                         />
                     )}
                 </div>
